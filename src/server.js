68d3b83742c3c3e9a6da1fb250c6c79985edb6de
GabrielGrld <gbrlgrld@gmail.com>
2021-12-08 16:01:59 -0500

    add urls path to server.js file

---
+++ src/server.js
@@ -0,0 +1,7 @@
+module.xports = {
+    auth_api_url: 'https://ts-auth-ms.herokuapp.com',
+    payment_apu_url:'https://ts-payment-ms.herokuapp.com',
+    pqr_api_url:'https://pqr-ms2021.herokuapp.com',
+    products_api_url:'https://products-ms2021.herokuapp.com/api/products' //only get method here
+
+};
